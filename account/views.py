99b1f800ac0ca54e092790bf43814571d65e459a
faridhaavasi <faridhavasi777@gmail.com>
2024-03-05 20:49:16 +0330

    test selootion ok

--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import RegisterSerializer
+from .models import User
 
-# Create your views here.
+class RegisterApiView(APIView):
+    serializer_class = RegisterSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+
+            request.session['register_info'] = {}
+            session_info_register = request.session['register_info']
+            session_info_register = {
+                'email': validated_data['email'],
+                'first_name': validated_data['first_name'],
+                'last_name': validated_data['last_name'],
+                'phone_number': validated_data['phone_number'],
+                'password': validated_data['password']
+            }
+            return Response(session_info_register)
+        return Response(serializer.errors)
+
+
+
