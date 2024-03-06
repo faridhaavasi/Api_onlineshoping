220661f0c7d4ab7735091bae455ed4bd9d06e5e1
faridhaavasi <faridhavasi777@gmail.com>
2024-03-06 22:13:08 +0330

    add jwt auth

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from. import views
-urlpatterns =[
-    path('register', views.RegisterApiView.as_view(), name='register_user'),
-    path('verify', views.VerifyCodeApi.as_view(), name='verify code'),
+from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/register', views.RegisterApiView.as_view(), name='register_user'),
+    path('api/verify', views.VerifyCodeApi.as_view(), name='verify code'),
 ]
