204d1dfe887077f0dec6d6c7a280c7943f1e7e7d
faridhaavasi <faridhavasi777@gmail.com>
2024-03-06 18:54:45 +0330

    register ok

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import RegisterSerializer
-from .models import User
+from rest_framework import status
+from random import randint
+from .models import User, Otp
 
 class RegisterApiView(APIView):
     serializer_class = RegisterSerializer
@@ -9,6 +11,7 @@ class RegisterApiView(APIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             validated_data = serializer.validated_data
+            random_code = randint(1000,9999)
 
             request.session['register_info'] = {}
             session_info_register = request.session['register_info']
@@ -17,10 +20,10 @@ class RegisterApiView(APIView):
                 'first_name': validated_data['first_name'],
                 'last_name': validated_data['last_name'],
                 'phone_number': validated_data['phone_number'],
-                'password': validated_data['password']
+                'password': validated_data['password'],
+                'random_code': random_code
             }
-            return Response(session_info_register)
+            Otp.objects.create(phone_number=validated_data['phone_number'], code=random_code)
+            return Response({'data':session_info_register}, status=status.HTTP_200_OK)
         return Response(serializer.errors)
-
-
 
