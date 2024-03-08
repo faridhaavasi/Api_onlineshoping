196bf5bc4a924085cb71dfcf48af43c9379ae5cf
faridhaavasi <faridhavasi777@gmail.com>
2024-03-08 22:36:57 +0330

    add permission and product api

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from product.views import ProductViewsetApi
+from rest_framework.routers import DefaultRouter
+
 app_name = 'product'
 urlpatterns = [
     path('category_list', views.CategoryListViewAPI.as_view(), name='category_list'),
 ]
 
+
+
+router = DefaultRouter()
+router.register(r'products', ProductViewsetApi, basename='product')
+urlpatterns = router.urls
--- product/views.py
+++ product/views.py
@@ -2,13 +2,32 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from .serializers import CategorySerializer, ProductSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions
 from .models import Category, Product
 
 class CategoryListViewAPI(APIView):
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
         instance = Category.objects.all()
         serializer = CategorySerializer(instance=instance)
+        return Response(serializer.data)
+
+
+
+class ProductViewsetApi(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    def list(self, request):
+        queryset = Product.objects.product_Avalible_True()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+    def retrieve(self, request, slug):
+        queryset = Product.objects.product_Avalible_True()
+        product = get_object_or_404(queryset, slug=slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
 
 
