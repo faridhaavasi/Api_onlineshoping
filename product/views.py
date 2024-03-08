ad5b09db0f5abacd7ccff92953c2522ac8a1f8ef
faridhaavasi <faridhavasi777@gmail.com>
2024-03-08 17:21:09 +0330

    category list view

--- product/views.py
+++ product/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from .serializers import CategorySerializer, ProductSerializer
+from .models import Category, Product
 
-# Create your views here.
+class CategoryListView(APIView):
+    serializer_class = CategorySerializer
+    def get(self, request):
+        instance = Category.objects.all()
+        serializer = CategorySerializer(instance=instance)
+        return Response(serializer.data)
+
+
+
+
+
+
