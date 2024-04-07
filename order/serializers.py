864e76d53bcbee74924adf3123a60bed277aa760
faridhaavasi <faridhavasi777@gmail.com>
2024-04-08 01:25:18 +0330

    add writ only in serializer

--- order/serializers.py
+++ order/serializers.py
@@ -2,5 +2,9 @@ from rest_framework import serializers
 
 
 class CartAddSerializer(serializers.Serializer):
-    id_of_product = serializers.IntegerField()
-    quantity = serializers.IntegerField()
+    id_of_product = serializers.IntegerField(write_only=True)
+    quantity = serializers.IntegerField(write_only=True)
+
+
+class CartRemoveSerializer(serializers.Serializer):
+    id_of_product = serializers.IntegerField(write_only=True)
