056ebf4ffbfb968c7560c9cc8639a35e3261bdb1
faridhaavasi <faridhavasi777@gmail.com>
2024-07-06 01:31:42 +0330

    add fixture setupe and test url ok

--- account/tests/test_urls.py
+++ account/tests/test_urls.py
@@ -3,20 +3,27 @@ from django.urls import reverse, resolve
 from account.views import RegisterApiView, VerifyCodeApi
 
 
-
 class Testurls(SimpleTestCase):
-    def test_registerApiView_url_name(self):
-        url = reverse('accounts:register_user')
-        url_name = resolve(url).url_name
+    def setUp(self):
+        self.register_url = reverse('accounts:register_user')
+        self.veryfi_url = reverse('accounts:verify code')
 
+
+    def test_registerApiView_url_name(self):
+        url_name = resolve(self.register_url).url_name
         self.assertEqual(url_name, 'register_user')
 
     def test_VerifycodeApi_url_name(self):
-        url = reverse('accounts:verify code')
-        url_name = resolve(url).url_name
-
+        url_name = resolve(self.veryfi_url).url_name
         self.assertEqual(url_name, 'verify code')
 
+    def test_RegisterApiView(self):
+        view = resolve(self.register_url).func.view_class
+        self.assertEqual(view, RegisterApiView)
 
+
+    def test_veryfiCodeApiView(self):
+        view = resolve(self.veryfi_url).func.view_class
+        self.assertEqual(view, VerifyCodeApi)
 
 
