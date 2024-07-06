17e88ba39c6030c1f56a305b3ac31ac145f63607
faridhaavasi <faridhavasi777@gmail.com>
2024-07-06 16:30:22 +0330

    add test models account

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -5,8 +5,11 @@ from account.models import User, Otp
 class TestModelsAccount(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(phone_number='09191234567', password='123')
+        self.otp = Otp.objects.create(phone_number='09191234567', code='1111')
 
     def test_str(self):
         self.assertEqual(str(self.user), '09191234567')
+        self.assertEqual(str(self.otp), 'user-09191234567-random-code1111')
+
 
 
