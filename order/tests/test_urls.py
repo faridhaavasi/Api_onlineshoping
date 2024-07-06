b6529d9f669108d9e8a5b0538c1cb9acdfe15fa8
faridhaavasi <faridhavasi777@gmail.com>
2024-07-06 11:59:33 +0330

    files test

--- order/tests/test_urls.py
+++ order/tests/test_urls.py
@@ -1,8 +1,15 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from order.views import OrderItemViewApi, AddOrderViewApi, CartViewApi, CartRemoveViewApi, CartAddViewApi
+from order.views import AddOrderViewApi, CartViewApi, CartRemoveViewApi, CartAddViewApi
 
 
 
 class TestOrderUrl(SimpleTestCase):
-    pass
+    def setUp(self):
+        self.AddOrderApiViewUrl = reverse('order:add_order')
+        self.CartViewApiUrl = reverse('order:cart')
+        self.CartAddApiViewUrl = reverse('order:cart_add')
+        self.CartRemoveApiViewUrl = reverse('order:cart_remove')
+    def test_AddOrderApiViewUrl(self):
+        view = resolve(self.AddOrderApiViewUrl).func.view_class
+        self.assertEqual(view, AddOrderViewApi)
