e8ac6cc7ef98cc781c18add496f43211fb79404a
faridhaavasi <faridhavasi777@gmail.com>
2024-07-06 11:25:49 +0330

    first class order test

---
+++ order/tests/test_urls.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from order.views import OrderItemViewApi, AddOrderViewApi, CartViewApi, CartRemoveViewApi, CartAddViewApi
+
+
+
+class TestOrderUrl(SimpleTestCase):
+    pass
