886fff9d13a24662dd846a474d27720517e8c24b
faridhaavasi <faridhavasi777@gmail.com>
2024-07-08 00:52:10 +0330

    set test

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -8,7 +8,7 @@ class TestAccountViews(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def Test_registerApiViewValidPostMethod(self):
+    def test_register_api_view_valid_post_method(self):
         ser = RegisterSerializer(data=
         {
             'phone_number': '09191234567',
@@ -18,8 +18,28 @@ class TestAccountViews(TestCase):
             'password': '1111'
         }
         )
-        req = self.client.post(reverse('accounts:register_user'), data=ser.data)
-        self.assertEqual(req.status_code, 200)
-        self.assertEqual(User.objects.count(),1)
+        if ser.is_valid():
+            req = self.client.post(reverse('accounts:register_user'), data=ser.data)
+            User.objects.create_user(**ser.data)
+            self.assertEqual(req.status_code, 200)
+            self.assertEqual(User.objects.count(),1)
 
+    # def test_register_api_view_invalid_post_method(self):
+    #     ser = RegisterSerializer(data=
+    #     {
+    #         'phone_number': '0919123456711',
+    #         'email': 'faridgmaul.com',
+    #         'first_name': 'farid',
+    #         'last_name': 'havasi',
+    #         'password': '1111'
+    #     }
+    #     )
+    #     req = self.client.post(reverse('accounts:register_user'), data=ser.data)
+    #     self.assertFalse(ser.is_valid())
+    #
+    '''
+    When the serializer is of the serializer model,
+     it is not tested at all if the data is not correct,
+     because the validation prevents the entry of wrong data.
+    '''
 
