8530ff2a3d9696921e1f802db2ca3f8b94dedd6c
faridhaavasi <faridhavasi777@gmail.com>
2024-07-11 17:25:52 +0330

    test model order

---
+++ order/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from order.models import Order, OrderItem, Coupon
+from django.contrib.auth import get_user_model
+User = get_user_model()
+class TestOrderModels(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(phone_number='08108548640', password='111234')
+        self.user_id = User.objects.get(id=1)
+        self.order = Order.objects.create(user=self.user_id, paid=True)
+    def test_order_model(self):
+        self.assertEqual(str(self.order), f'{self.order.user}- True')
